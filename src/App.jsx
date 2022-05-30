6454a465bff54c8becd9442d2527c736e3548be5
Akib Ali <akib7599@gmail.com>
2022-05-30 21:24:06 +0530

    working fine

--- src/App.jsx
+++ src/App.jsx
@@ -4,67 +4,58 @@ import logo from './logo.svg'
 import './App.css'
 import CandidateCard from "./components/CandidateCard";
 import Button from "./components/Button";
-//import { sortUserPlugins } from "vite";
 
 function App() {
- 
- 
   const[data,setdata] = useState([])
   const [error, setError] = useState(false)
   const [page, setPage] = useState(1)
   const [loading,setLoading] = useState(true)
-  const [sort,setSort]  = useState("DESC")
 
 useEffect(()=>{
-setLoading(true)
-setError(false)
-  Fetchdata()
-},[page,sort])
+  setLoading(true)
+  Fetchdata({page})
+},[page])
 
 
 
-  const Fetchdata=()=>{
+  const Fetchdata=({page})=>{
 
    axios({
-    
      method:"get",
      url:"https://json-server-mocker-masai.herokuapp.com/candidates",
      //url:" http://localhost:8080/users",
      params:{
           _page:page,
-          _limit:5,
-          _sort:'salary',
-          order:sort
+          _limit:5
 
      }
      
    })
    .then((res)=>{
-
   setdata(res.data)
   setLoading(false)
-   }).catch((err)=> {
+   }).catch(err=> {
      setError(true)
      setLoading(false)
        
    })
   }
-
-
+  
 
   return (
     <div className="App">
   
     <div>
-     {loading && <div id="loading-container"> ...loading </div> }
-       {error &&  <div> ...error </div>}
+       {loading && 
+        <div id="loading-container"> ...loading
 
-          <Button disabled={ sort ==='ASC'} id="SORT_BUTTON" title={`Sort by Ascending Salary`}  onClick={()=> setSort("ASC")}/>  
+        </div> }
+          <Button id="SORT_BUTTON" title={`Sort by Ascending Salary`} />  
          
-        <Button disabled={page === 1} onClick={()=> setPage(page -1)} title="PREV" id="PREV" />
+        <Button disabled={page == 1} onClick={()=> setPage(page -1)} title="PREV" id="PREV" />
         
 
-        <Button  onClick={()=> setPage(page +1)} id="NEXT" title="NEXT" /> 
+        <Button onClick={()=> setPage(page +1)} title="NEXT" /> 
       </div>
       {data.map((item) =>  <CandidateCard key={item.id} {...item}/>
         
